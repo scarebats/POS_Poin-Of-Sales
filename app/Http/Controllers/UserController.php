b033545bf2e4c71b871d969f43cd86a9d4aac7f0
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2025-03-16 12:51:36 +0700

    J3 - Prak 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+              'nama' => 'Pelanggan Pertama',
+        //    'username' => 'customer-1',
+        //    'nama' => 'Pelanggan',
+        //    'password' => Hash::make('12345'),
+        //    'level_id' => 4
+        ];
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+
     public function show($id, $name)
     {
         return view('user.profile', compact('id', 'name'));
