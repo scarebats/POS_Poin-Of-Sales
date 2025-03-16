a1379ae4e7a6855dc0a43aa879980b6fda22efb2
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2025-03-16 16:23:55 +0700

    J4 - Prak 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,20 +9,32 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-    {
-        // tambah data user dengan Eloquent Model
+    {   
         $data = [
-              'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+
+
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //    'nama' => 'Pelanggan Pertama',
         //    'username' => 'customer-1',
         //    'nama' => 'Pelanggan',
         //    'password' => Hash::make('12345'),
         //    'level_id' => 4
-        ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-        UserModel::where('username', 'customer-1')->update($data); // update data user
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // ];
+        // // UserModel::insert($data); // tambahkan data ke tabel m_user
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
     }
 
     public function show($id, $name)
