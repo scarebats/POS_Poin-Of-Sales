6eb8feae3ef5f8d3b075572cc38a2defb6fbd7a9
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2025-06-15 21:00:40 +0700

    J10 - Praktikum 1 dan 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Http\Request;
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', 'App\Http\Controllers\Api\RegisterController');
+Route::post('/login', 'App\Http\Controllers\Api\LoginController');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
