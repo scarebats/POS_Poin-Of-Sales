492e5adcde8c955d08ba94ada593fc363997a5f7
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2025-06-15 18:06:34 +0700

    J7

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('m_user')->updateOrInsert(
+            ['username' => 'admin'], // Kondisi: jika sudah ada admin
+            [
+                'level_id' => 1,
+                'nama' => 'Administrator',
+                'password' => Hash::make('admin123'), // Password default
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
     }
 }
