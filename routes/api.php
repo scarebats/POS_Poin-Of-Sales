2cac0d429d3a00f32283efcd0cbe98acd20fcc6b
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2025-06-15 21:24:29 +0700

    J10 -Praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', 'App\Http\Controllers\Api\RegisterController');
 Route::post('/login', 'App\Http\Controllers\Api\LoginController');
 Route::post('/logout', 'App\Http\Controllers\Api\LogoutController');
+Route::get('levels',          [LevelController::class, 'index']);
+Route::post('levels',         [LevelController::class, 'store']);
+Route::get('levels/{level}',  [LevelController::class, 'show']);
+Route::put('levels/{level}',  [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
